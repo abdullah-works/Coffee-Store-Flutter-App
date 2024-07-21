4543e5ccea3c9f294deff191ed9075c43d14562b
abdullah-works <abdullah.worke@gmail.com>
2024-07-21 17:47:18 +0500

    implemented a custom search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 28),
         child: Column(
           children: [
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -49,7 +54,65 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ],
-            )
+            ),
+            const SizedBox(height: 20),
+            SearchAnchor(
+              // viewBackgroundColor: Colors.white, Default
+              builder: (context, controller) {
+                return TextField(
+                  controller: controller,
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Search Coffee',
+                      hintStyle: TextStyle(
+                        height: 3.5,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      prefixIcon: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                        child: Icon(
+                          Icons.search,
+                          size: 32,
+                        ),
+                      ),
+                      suffixIcon: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                        child: Icon(
+                          Icons.filter_alt,
+                          size: 32,
+                        ),
+                      )),
+                  // side: null,
+                );
+              },
+              suggestionsBuilder: (context, controller) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'Coffee Item $index';
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      setState(() {
+                        controller.closeView(item);
+                      });
+                    },
+                  );
+                });
+              },
+            ),
+            const SizedBox(height: 20),
           ],
         ),
       ),
